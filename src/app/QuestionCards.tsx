064ebf803428636d23687bc6d16103cb96e3416e
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-11-28 13:12:00 +0100

    done with it

--- src/app/QuestionCards.tsx
+++ src/app/QuestionCards.tsx
@@ -1,40 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import { AnswerObject } from "../App";
+import { QuestionWrapper, ButtonWrapper } from "./QuestionCards.styles";
 
 type Props = {
-    question: string;
-    answers: string[];
-    callback: any;
-    userAnswer: boolean;
-    questionNr: number;
-    totalQuestions: number;
-  };
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
 const QuestionCards: React.FC<Props> = ({
-    question,
-    answers,
-    callback,
-    userAnswer,
-    questionNr,
-    totalQuestions,
-  }) => (
-
-        <div>
-            <p className="number">
-                Question: {questionNr} / {totalQuestions}
-            </p>
-            <p dangerouslySetInnerHTML={{ __html: question }}/>
-            <div>
-                {answers.map(answer => (
-                    <div>
-                        <button disabled={userAnswer} onClick={callback}>
-                            <span dangerouslySetInnerHTML={{ __html: answer}}/>
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <QuestionWrapper>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <ButtonWrapper
+            key={answer}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
+            <button
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </ButtonWrapper>
+        ))}
+      </div>
+    </QuestionWrapper>
+  </div>
 );
 
 export default QuestionCards;
