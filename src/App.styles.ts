1eba8a1af011def74028d7fe8d85a11c95d28d49
B3ns44d <abdsamad23bnsaad@gmail.com>
2020-12-09 12:58:48 +0000

    fix deploy error again

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from 'styled-components';
 import BackgroundIMG from "./img/bg.png";
 
 export const AppStyle = createGlobalStyle`
